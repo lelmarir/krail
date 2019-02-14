da79760b31c97cd696abd722468b010955412442
lelmarir <lelmarir@gmail.com>
2019-02-14 12:24:19 +0100

    fix: ignore VaadinSession if other avalible

--- src/main/java/uk/q3c/krail/core/shiro/SecuritySessionModule.java
+++ src/main/java/uk/q3c/krail/core/shiro/SecuritySessionModule.java
@@ -9,6 +9,7 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.binder.LinkedBindingBuilder;
 import com.google.inject.multibindings.Multibinder;
+import com.vaadin.server.VaadinSession;
 
 public class SecuritySessionModule extends AbstractModule {
 
@@ -35,15 +36,22 @@ public class SecuritySessionModule extends AbstractModule {
 		for (SessionProvider sessionProvider : sessionProviders) {
 			SecuritySession s = sessionProvider.get();
 			if (s != null) {
-				if (LOGGER.isDebugEnabled()) {
-					if (session != null) {
-						//FIXME: non dovrebbero esservi mai 2 sessioni, ma sembra che il thread arrivi sporco e quindi trovo anche la VaadinSession
-						LOGGER.error("More than one SessionProvider returned a session: " + session + " and " + s + "");
+				if (session != null) {
+					// FIXME: non dovrebbero esservi mai 2 sessioni, ma sembra che il thread arrivi
+					// sporco e quindi trovo anche la VaadinSession
+					LOGGER.error("More than one SessionProvider returned a session: " + session + " and " + s + "");
+					// anche se qualcosa non va, do la precedenza alla sessione http e scarto la
+					// sessione di Vaadin
+					if (session instanceof VaadinSecuritySession) {
+						return s;
+					} else if (s instanceof VaadinSecuritySession) {
+						return session;
+					} else {
+						throw new AssertionError(
+								"Nessuna delle due sessioni è una vaadin session, quindi questo caso non è contemplato: è stato aggiunto un nuovo session provider?");
 					}
-					session = s;
-				} else {
-					return s;
 				}
+				session = s;
 			}
 		}
 		return session;
